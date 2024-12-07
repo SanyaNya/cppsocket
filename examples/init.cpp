ec4f47c5542a98f1dc666d982f5edd3026681984
SanyaNya <sanyanya4ever@gmail.com>
2024-12-07 18:53:18 +0300

    Reporting system error just like number

--- examples/init.cpp
+++ examples/init.cpp
@@ -5,15 +5,5 @@ int main()
 {
   auto net = cpps::Net::make<ehl::Policy::Expected>();
   if(!net)
-  {
-    auto ge = static_cast<cpps::GenericInitError>(net.error());
-    if(ge == cpps::GenericInitError::SystemNotReady)
-    {
-      std::cout << "System not ready!\n";
-    }
-    else if(ge == cpps::GenericInitError::ProcessLimit)
-    {
-      std::cout << "Process limit!\n";
-    }
-  }
+    std::cout << "System Error #" << net.error() << std::endl;
 }
