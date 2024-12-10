c955a1c282d20bbd1c1bbc7aa64dc3e1031594cf
SanyaNya <sanyanya4ever@gmail.com>
2024-12-10 18:13:35 +0300

    Update system_errc to v0.2.0 and use error message

--- examples/init.cpp
+++ examples/init.cpp
@@ -5,5 +5,5 @@ int main()
 {
   auto net = cpps::Net::make<ehl::Policy::Expected>();
   if(!net)
-    std::cout << "System Error #" << net.error() << std::endl;
+    std::cout << "System Error: " << net.error().message().data() << std::endl;
 }
