b496fdd3f6a010bf6c05dc5d65b05817a5d56e31
SanyaNya <sanyanya4ever@gmail.com>
2024-12-21 11:42:13 +0300

    Update example

--- examples/init.cpp
+++ examples/init.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
-  auto net = cpps::Net::make<ehl::Policy::Expected>();
-  if(!net)
-    std::cout << "System Error: " << net.error().message().data() << std::endl;
+  constexpr auto EHP = ehl::Policy::Unreachable;
+  auto net = cpps::Net::make<EHP>();
+  auto sock = net.socket<cpps::AddressFamily::IPv4, cpps::SocketType::Stream, cpps::SocketProtocol::TCP, EHP>();
+  constexpr auto addr = cpps::Address<cpps::AddressFamily::IPv4>::make<EHP>("192.168.1.50", 6969);
+  sock.bind<EHP>(addr);
+  sock.listen<EHP>(10);
+  auto conn = sock.accept<EHP>();
 }
